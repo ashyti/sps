eb1b2d6482155f745f4b041be95d9c40bde475df
Guarecuco <daniel.guarecuco@gmail.com>
2022-02-08 15:23:13 +0100

    Adding some comments

--- applications/target.h
+++ applications/target.h
@@ -9,7 +9,7 @@
 #define TARGET_STACK_SIZE       512
 #define TARGET_PRIORITY         1
 #define TARGET_TICK             1
-#define TARGET_FREEZE_PROB      2 //2%
+#define TARGET_FREEZE_PROB      2       /**< 2% Freeze Probability */
 
 enum pwr_status{ON, OFF, FROZEN};
 
@@ -19,15 +19,13 @@ struct target
 
     rt_mailbox_t mb_gpio;               /**< GPIO mailbox */
     rt_mailbox_t mb_ping;               /**< Ping mailbox -From SPS*/
-    rt_mailbox_t mb_ping_ack;            /**< Ping ACK mailbox -To SPS*/
-    rt_uint8_t id;
-    enum pwr_status status;
+    rt_mailbox_t mb_ping_ack;           /**< Ping ACK mailbox -To SPS*/
+    rt_uint8_t id;                      /**< Address */
+    enum pwr_status status;             /**< Power Status */
 };
 typedef struct target *target_t;
 
 extern struct target target[SPS_NUM_TARGETS];
-
-
 
 rt_uint8_t do_i_freeze(rt_uint8_t probability);
 static void simulation_thread_entry(void *param);
