866829c4ffb48ae32d4f0f7bbdb0a9adb49cb521
Daniel Guarecuco <daniel.guarecuco@studenti.polito.it>
2022-02-14 01:18:20 +0100

    sps: add SPS Smart Power Switch implementation
    The SPS is a Smart Power Switch that interfaces one one end with a user
    and with a given number of targets on the other end. Throught the SPS
    the user can control the power status of targets deciding whether he
    wants to shut them down or power them on.
    
    Add the main implementation of it. The SPS is currently handled by a
    main() function which triggers its initialization and startup. The
    library returns an SPS reference.
    
    The SPS checks for the status of the target machines and wait for the
    user to send power on/off commands.
    
    Signed-off-by: Daniel Guarecuco <daniel.guarecuco@studenti.polito.it>
    Signed-off-by: Andi Shyti <andi.shyti@studenti.polito.it>

---
+++ applications/sps.h
@@ -0,0 +1,30 @@
+#ifndef __SPS_H__
+#define __SPS_H__
+
+#include "main.h"
+
+struct sps {
+    rt_thread_t irq_in_handler;
+    rt_thread_t irq_out_handler;
+    rt_thread_t ping_thread;
+
+    rt_mailbox_t irq_out;
+    rt_mailbox_t irq_in;
+    rt_mailbox_t mb_ping[SPS_NUM_TARGETS];
+    rt_mailbox_t mb_ping_ack;
+    rt_mailbox_t mb_ping_irq;
+    rt_mailbox_t gpio[SPS_NUM_TARGETS];
+
+    rt_mutex_t target_mutex; /* guards the targets */
+    enum sps_pwr_status targets[SPS_NUM_TARGETS];
+
+    rt_uint8_t targets_pong[SPS_NUM_TARGETS];
+    rt_uint8_t targets_feedback[SPS_NUM_TARGETS];
+};
+typedef struct sps *sps_t;
+
+sps_t sps_init(void);
+rt_err_t sps_start(sps_t sps);
+void sps_delete(sps_t sps);
+
+#endif
