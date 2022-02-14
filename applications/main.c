816814b286500decf998539f54d91a040460dc20
Andi Shyti <andi.shyti@studenti.polito.it>
2022-02-14 01:21:24 +0100

    host: Add the simulation of a host usage
    The host part of this project simulates the behavior of a user. It's
    made essentially of two parts:
    
    1. The first part is the decision making where the user, in a loop,
       chooses whether to power on or off (i.e. change power status)
       a machine with a probability of 10%. The command is sent through
       i2c.
    
    2. The user is always notified when a target is frozen. An IRQ
       is sent by the SPS.
    
    Both the operations are synchronized through two irq lines
    (simulated by RT-Thread mailboxes) for each of the two situation.
    
    Signed-off-by: Andi Shyti <andi.shyti@studenti.polito.it>
    Reviewed-by: Daniel Guarecuco <daniel.guarecuco@studenti.polito.it>

--- applications/main.c
+++ applications/main.c
@@ -3,11 +3,13 @@
 #include <stdlib.h>
 
 #include "main.h"
+#include "host.h"
 #include "sps.h"
 
 int main(void)
 {
     sps_t sps;
+    host_t host;
     rt_err_t ret;
 
     printf("Welcome to SPS!\n");
@@ -21,10 +23,22 @@ int main(void)
         return RT_ERROR;
     }
 
+    host = host_init(sps->irq_out, sps->irq_in);
+    if (!host) {
+        printf("Failed to create host\n");
+        ret = RT_ERROR;
+        goto clean_sps;
+    }
 
     ret = sps_start(sps);
     if (ret) {
         printf("Failed to start SPS");
+        goto error_exit;
+    }
+
+    ret = host_start(host);
+    if (ret) {
+        printf("Failed to start host");
         goto error_exit;
     }
 
